cccc11913272320d1e620826d333345146b5017a
GlennBarosen <glennbarosen@gmail.com>
2020-04-23 12:58:25 +0200

    TerningKopp ferdig 🥤

--- no/hvl/dat109/objects/TerningKopp.java
+++ no/hvl/dat109/objects/TerningKopp.java
@@ -1,5 +1,18 @@
 package no.hvl.dat109.objects;
 
 public class TerningKopp {
+	
+	Terning t1;
+	Terning t2;
+	
+	
+	public TerningKopp(Terning t1, Terning t2) {
+		t1 = new Terning();
+		t2 = new Terning();
+	}
+	
+	public int trillTerninger() {
+		return t1.trill() + t2.trill();
+	}
 
 }
