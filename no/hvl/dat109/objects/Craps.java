3772e4330e591bcb3bfa68399db41e35f859508a
GlennBarosen <glennbarosen@gmail.com>
2020-04-23 11:53:13 +0200

    Lagt til klasser 🔥

---
+++ no/hvl/dat109/objects/Craps.java
@@ -0,0 +1,10 @@
+package no.hvl.dat109.objects;
+
+public class Craps {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ no/hvl/dat109/objects/Terning.java
@@ -0,0 +1,5 @@
+package no.hvl.dat109.objects;
+
+public class Terning {
+
+}
